fed7d5ead870a9844935168f3f0d235ac147bfb5
Mofitex <32845654+Mofitex@users.noreply.github.com>
2019-03-26 18:29:52 +0100

    Add files via upload

--- config/routes.rb
+++ config/routes.rb
@@ -7,5 +7,11 @@ Rails.application.routes.draw do
     get "delete"
   end
   devise_for :users
+  resources :api do
+    collection do
+      get "animals" => 'api#animals'
+      get "zonas" => 'api#zonas'
+    end
+  end
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
